c92f8b2d4db5f936a0c1b38ee8ed1695a1c5ae78
Ivan Fedorenko <81463720+fedorenkoivan@users.noreply.github.com>
2024-11-22 01:40:07 +0200

    done all problems

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+    const array = [];
+    const iwantto = (n) => array[n];
+    iwantto.push = (e) => array.push(e);
+    iwantto.pop = () => array.pop();
+
+    return iwantto;
+};
 
 module.exports = { array };
